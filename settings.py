28a34ee91eb1e15f8c6cac7875d28e0473d7aadc
Chaoran Wang <superchaoran@gmail.com>
2017-10-09 07:51:50 +0000

    updating qun and time logic

--- settings.py
+++ settings.py
@@ -1,20 +1,21 @@
 # -*- coding: UTF-8 -*-
+import datetime
 def init():
   global admins
   global chatGroups
   global vT
   global usersDict
   global ADMIN
+  global previousDay
 
   chatGroups =[ 
-  u"天天刷题",u"天天健身",
+  u"leetcode天天",u"天天健身",
   u"北美CPA",u"Stanford湾区桌游",
   u'Stanford湾区二手车',u'Stanford湾区二手货',
   u'Stanford湾区租房',u'Stanford旧金山租房',
-  u'Stanford湾区厨艺',u'天天剁手',
+  u'Stanford湾区厨艺',u'北美游戏交流',
   u'Stanford湾区护肤品',u'北美妈妈',
-  u'2017湾区实习工作群',u'Stanford校友群',
-  u'北美信用',u'找莹颖',
+  u'Stanford硅谷工作内推',u'北美信用',
   u'Stanford,UCD',u'cmu湾区行李保管',
   u'线上KTV',u'北美表情分享'
   ]
@@ -25,15 +26,16 @@ def init():
   v2= u"回复 1 北美CPA,REG刷题群;桌游群\n"
   v3= u"回复 2 加湾区、旧金山二手货、车群;\n"
   v4= u"回复 3 加湾区、旧金山租房群.\n"
-  v5= u"回复 4 加湾区厨艺分享;戒游戏群\n"
-  v6= u"回复 5 加湾区护肤品化妆品.加北美母婴总群.\n"
-  v7= u"回复 6 加湾区实习工作群；Stanford校友群，非校友请勿进此群，谢谢.\n"
-  v8= u"回复 7 加北美信用卡爱好者；Finding Yingying 群，家人校友都在努力～.\n"
-  v9= u"回复 8 加cmu湾区行李保管中美互运、Stanford,UCD拼车搭车群.\n"
-  v10= u"回复 9 加线上KTV开嗓🎙️北美总群;北美表情分享总群\n"
+  v5= u"回复 4 加湾区厨艺分享;北美游戏交流总群\n"
+  v6= u"回复 5 加湾区护肤品化妆品;加北美母婴总群.\n"
+  v7= u"回复 6 Stanford硅谷工作内推群;北美信用卡爱好者群.\n"
+  v8= u"回复 7 加cmu湾区行李保管中美互运;Stanford,UCD拼车搭车群.\n"
+  v9= u"回复 8 加线上KTV开嗓🎙️北美总群;北美表情分享总群\n"
   v11= u"回复 99 查看【北美加群小助手Jogchat.com】\n微信公众号二维码加纽约、芝加哥、三番、西雅图等群\n"
-  vT =v0+v00+v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11
+  vT =v0+v00+v1+v2+v3+v4+v5+v6+v7+v8+v9+v11
 
   usersDict = {}
   admins=[]
   ADMIN = u'Stanford湾区加群助手'
+  previousDay = datetime.datetime.now().day
+
