9125d99590b1c6b8590d7d2075b5e313619f911d
Chaoran Wang <superchaoran@gmail.com>
2017-11-12 21:40:59 +0000

    adding Chuck Guo

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ def init():
   u'Stanford硅谷工作内推',u'北美信用',
   u'Stanford,UCD',u'cmu湾区行李保管',
   u'线上KTV',u'北美表情分享',
-  u'开战leetcode'
+  u'开战leetcode',u'Chuck郭律师'
   ]
 
   v0= u"您好，😊Stanford加群建群小助手😊为您服务～\n"
@@ -32,7 +32,7 @@ def init():
   v7= u"回复 6 Stanford硅谷工作内推群;北美信用卡爱好者群.\n"
   v8= u"回复 7 加cmu湾区行李保管中美互运;Stanford,UCD拼车搭车群.\n"
   v9= u"回复 8 加线上KTV开嗓🎙️北美总群;北美表情分享总群.\n"
-  v10= u"回复 9 加开战leetcode群(每周一战）。\n"
+  v10= u"回复 9 加开战leetcode群(每周一战);加Chuck郭律师美帝绿卡讨论群.\n"
   v11= u"回复 99 查看【北美加群小助手Jogchat.com】\n微信公众号二维码加纽约、芝加哥、三番、西雅图等群\n"
   vT =v0+v00+v1+v2+v3+v4+v5+v6+v7+v8+v9+v10+v11
 
